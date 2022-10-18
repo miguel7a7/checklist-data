b3ab0224bb39e20b01f59a7f66cb9495526e7484
webmaick <mickmiguel05@gmail.com>
2022-10-18 16:34:10 -0400

    habilitando pagina

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { Error404 } from "./404/Error404";
-import "./App.css";
-import { ListProvider } from "./context/ListContext";
-// import { RouterApp } from './router/RouterApp';
+import './App.css'
+import { ListProvider } from './context/ListContext'
+import { RouterApp } from './router/RouterApp'
+// import { Error404 } from "./404/Error404";
 
 function App() {
   return (
     <ListProvider>
-      <Error404 />
-      {/* <RouterApp /> */}
+      {/* <Error404 /> */}
+      <RouterApp />
     </ListProvider>
-  );
+  )
 }
 
-export default App;
+export default App
